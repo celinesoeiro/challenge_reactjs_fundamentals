22d9f077213a0fc7b2739fc8f2b49324b911f2a2
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2020-10-05 11:41:58 -0300

    solved

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,30 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
+    const file = uploadedFiles[0];
 
-    // TODO
+    if (!file) return;
+
+    data.append('file', file.file, file.name);
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+
+    const uploadedFile = files.map(file => ({
+      readableSize: filesize(file.size),
+      name: file.name,
+      file: file,
+    }));
+
+    setUploadedFiles(uploadedFile);
   }
 
   return (
